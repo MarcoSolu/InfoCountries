0ebbf1855c78f17c2fd95f79ddf2b2e10e881b98
Marco Solustri <solustri00@gmail.com>
2023-10-04 23:51:12 +0200

    upload

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({ 
-  origin: "http://localhost:3000",
+  origin: "https://info-countries-eight.vercel.app/",
   credentials: true  
 }));
 
