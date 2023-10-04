33c4c675dd1c6cf778235efecbaaea4fd0a8c76e
Marco Solustri <solustri00@gmail.com>
2023-10-05 00:10:14 +0200

    update

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({ 
-  origin: "https://info-countries-eight.vercel.app/",
+  origin: 'https://info-countries-eight.vercel.app/',
   credentials: true  
 }));
 
