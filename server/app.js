f4eae9bf8175d0fcab52658fe71680eedd6b9dba
Marco Solustri <solustri00@gmail.com>
2023-10-16 19:54:32 +0200

    update

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const User = require('./models/User');
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const verifyToken = require('./auth');
 
 const app = express();
 
