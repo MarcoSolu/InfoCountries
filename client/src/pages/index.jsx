684d1c9fc0f2de66b2970fd9bea3a64eeda1111f
Marco Solustri <solustri00@gmail.com>
2023-10-21 19:09:54 +0200

    upload

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import JoinDiv from './components/JoinDiv';
+import Navbar from '../components/Navbar';
+import JoinDiv from '../components/JoinDiv';
 
 const Home = () => {
   return (
